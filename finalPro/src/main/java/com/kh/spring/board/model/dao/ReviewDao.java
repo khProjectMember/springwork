1494a16d97329717425798a4caec598257325977
substribkangho <114133304+substribkangho@users.noreply.github.com>
2023-01-24 12:34:34 +0900

    rt
    rt

--- finalPro/src/main/java/com/kh/spring/board/model/dao/ReviewDao.java
+++ finalPro/src/main/java/com/kh/spring/board/model/dao/ReviewDao.java
@@ -1,8 +1,109 @@
 package com.kh.spring.board.model.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.spring.board.model.vo.Review;
+import com.kh.spring.board.model.vo.ReviewReply;
+import com.kh.spring.board.model.vo.Reviewrecommend;
+import com.kh.spring.common.model.vo.PageInfo;
 
 @Repository
 public class ReviewDao {
-
+	public int reviewListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("reviewMapper.reviewListCount");
+	}
+	
+	public ArrayList<Review> selectReviewList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int startNo = (pi.getNowPage()-1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(startNo, limit);
+		return (ArrayList) sqlSession.selectList("reviewMapper.selectReviewList", null, rowBounds);
+	}
+	
+	public int insertReview(SqlSessionTemplate sqlSession, Review r) {
+		return sqlSession.insert("reviewMapper.insertReview", r);
+	}
+	
+	public int incCouont(SqlSessionTemplate sqlSession, int revNo) {
+		return sqlSession.update("reviewMapper.incCount", revNo);
+	}
+	
+	public Review selectReviewDetail(SqlSessionTemplate sqlSession, int revNo) {
+		return sqlSession.selectOne("reviewMapper.selectReviewDetail", revNo);
+	}
+	
+	
+	public int updateReview(SqlSessionTemplate sqlSession, Review r) {
+		return sqlSession.update("reviewMapper.updateReview", r);
+	}
+	
+	public int deleteReview(SqlSessionTemplate sqlSession, int revNo) {
+		return sqlSession.delete("reviewMapper.deleteReview", revNo);
+	}
+	
+	public int insertRecommend(SqlSessionTemplate sqlSession, int revNo, int memNo) {
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		
+		map.put("revNo", revNo);
+		map.put("memNo", memNo);
+		return sqlSession.insert("reviewMapper.insertRecommend", map);
+	}
+	
+	public int updateRecommendCount(SqlSessionTemplate sqlSession, int revNo) {
+		return sqlSession.update("reviewMapper.updateRecommendCount", revNo);
+	}
+	
+	public ArrayList<Reviewrecommend> selectRecommend(SqlSessionTemplate sqlSession, int revNo) {
+		return (ArrayList) sqlSession.selectList("reviewMapper.Reviewrecommend", revNo);
+	}
+	
+	public ArrayList<Review> selectViewNewDate(SqlSessionTemplate sqlSession, PageInfo pi) {
+		return (ArrayList) sqlSession.selectList("reviewMapper.selectViewNewDate", pi);
+	}
+	
+	public ArrayList<Review> selectViewManyDate(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int startNo = (pi.getNowPage()-1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(startNo, limit);
+		return (ArrayList) sqlSession.selectList("reviewMapper.selectViewManyDate", null, rowBounds);
+	}
+	
+	public ArrayList<Review> selectViewCountDate(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int startNo = (pi.getNowPage()-1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(startNo, limit);
+		return (ArrayList) sqlSession.selectList("reviewMapper.selectViewCountDate", null, rowBounds);
+	}
+	
+	public int searchCount(SqlSessionTemplate sqlSession, String keyvalue, String keyword) {
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("keyvalue", keyvalue);
+		map.put("keyword", keyword);
+		
+		return sqlSession.selectOne("reviewMapper.searchCount", map);
+	}
+	
+	public ArrayList<Review> selectSearchReview(SqlSessionTemplate sqlSession, PageInfo pi, String keyvalue, String keyword) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("keyvalue", keyvalue);
+		map.put("keyword", keyword);
+		
+		int startNo = (pi.getNowPage()-1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(startNo, limit);
+		
+		return (ArrayList) sqlSession.selectList("reviewMapper.selectSearchReview", map, rowBounds);
+	}
+	
+	public ArrayList<ReviewReply> reviewReplyList(SqlSessionTemplate sqlSession, int revNo) {
+		return (ArrayList) sqlSession.selectList("reviewMapper.reviewReplyList", revNo);
+	}
 }
+
